7f96db502acdef288f1163f8d7a40b2f74300655
Nikita Ivanov <nivanov.germany@gmail.com>
2024-08-16 18:48:04 +0200

    added hyperlink to translator page

--- home.py
+++ home.py
@@ -10,10 +10,10 @@ No personal data is stored by this website. There are no cookies or logs.
 
 ## Projects
 You can find currently available projects at the left sidebar.
-- Neural Machine Translation ([repo](https://github.com/nikita-ivanov/neural_translation))
+- <a href='translator' target='_self'>Translator</a>
 - coming soon...
 
 ## Contact
 - [GitHub](https://github.com/nikita-ivanov/koala)
 - [LinkedIn](https://linkedin.com/in/nikitaivanovde)
-""")
+""", unsafe_allow_html=True)
