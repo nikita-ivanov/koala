1c854f1980c9ccf022f4eaaa4b56b7f1c9353325
Nikita Ivanov <nivanov.germany@gmail.com>
2024-08-17 14:19:43 +0200

    added sequence length warning to the translator description

--- pages/📚_translator.py
+++ pages/📚_translator.py
@@ -18,6 +18,8 @@ It is based on an encoder-decoder transformer architecture.
 The model was trained on 200k German-English sentence pairs from an ISWLT [dataset](https://huggingface.co/datasets/IWSLT/iwslt2017).
 The model is tiny by current standards. It contains 10M parameters and its weight is 40MB.
 Therefore, translations might not be accurate. It works best with short sentences.
+Note: maximum sequence length is 128 tokens (roughly 100 words).
+Longer input sequences will be trimmed.
 The source code for training the model can be found [here](https://github.com/nikita-ivanov/neural_translation/blob/main/translator_transformer.ipynb).
 """)
 
