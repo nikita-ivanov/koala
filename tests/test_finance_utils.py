25c21fb808750d67f0eaad87a6cf3f617716368a
Nikita Ivanov <nivanov.germany@gmail.com>
2024-08-22 21:53:29 +0200

    added finance test

---
+++ tests/test_finance_utils.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+import pandas as pd
+import pytest
+
+from tools.finance_utils import sim_payoff_bootstrapped
+
+sample_series = pd.Series({
+        pd.to_datetime("2013-12-31"): 0.25,
+        pd.to_datetime("2014-12-31"): 0.42,
+        pd.to_datetime("2015-12-31"): -0.25,
+        pd.to_datetime("2016-12-31"): -0.33,
+        pd.to_datetime("2016-12-31"): 0.02,
+    })
+
+@dataclass
+class TestParameters:
+    series: pd.Series = sample_series
+    start_value = 1.42
+
+parameters = TestParameters()
+
+@pytest.mark.parametrize("mean", [sample_series.mean(), 0.05])
+@pytest.mark.parametrize("volatility", [sample_series.std(), 0.24])
+def test_sim_payoff(mean: float, volatility: float) -> None:
+    num_scenarios = 100
+
+    simulated_payoff = sim_payoff_bootstrapped(hist_returns=sample_series,
+                                               num_scenarios=num_scenarios,
+                                               start_value=parameters.start_value,
+                                               mean=mean,
+                                               volatility=volatility)
+
+    assert simulated_payoff.mean() == pytest.approx(mean * parameters.start_value)
+    assert simulated_payoff.std() == pytest.approx(volatility * parameters.start_value)
+    assert len(simulated_payoff) == num_scenarios
